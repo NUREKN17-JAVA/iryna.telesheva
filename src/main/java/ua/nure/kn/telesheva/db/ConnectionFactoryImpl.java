121706cb4d4863a6a2bec75e83baaae369f4e13d
Telesheva <megoirlik11@gmail.com>
2019-10-10 09:54:52 +0300

    Creating of ConnectionFactory

---
+++ src/main/java/ua/nure/kn/telesheva/db/ConnectionFactoryImpl.java
@@ -0,0 +1,31 @@
+package ua.nure.kn.telesheva.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ConnectionFactoryImpl implements ConnectionFactory {
+
+	@Override
+	public Connection getConnection() throws DatabaseException {
+		String driver = "org.hsqldb.jdbcDriver";
+		String url = "jdbc:hsqldb:file:db/usermanagement";
+		String user = "sa";
+		String password = "";
+		
+		//рефлексия
+		
+		try {
+			Class.forName(driver);
+		} catch (ClassNotFoundException e) {
+			throw new DatabaseException(e);
+		}
+		
+		try {
+			return DriverManager.getConnection(url, user, password);
+		} catch (SQLException e) {
+			throw new DatabaseException(e);
+		}
+	}
+
+}
