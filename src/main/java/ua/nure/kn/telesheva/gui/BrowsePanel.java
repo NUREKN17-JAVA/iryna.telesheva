d8fcb8c710a2eac0ebbf8766e117047a347a498e
Telesheva <megoirlik11@gmail.com>
2019-12-03 13:31:42 +0200

    Creating the main frame and control panel

---
+++ src/main/java/ua/nure/kn/telesheva/gui/BrowsePanel.java
@@ -0,0 +1,11 @@
+package ua.nure.kn.telesheva.gui;
+
+import javax.swing.JPanel;
+
+public class BrowsePanel extends JPanel {
+
+	public BrowsePanel(MainFrame frame) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
